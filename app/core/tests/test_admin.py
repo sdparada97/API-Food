ab8d057c9b9d552595ce02f88b8ea1633a23d9de
sdparada97 <sdparada97@gmail.com>
2022-10-20 00:09:27 -0500

    Se agrega personalización de Admin

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -14,7 +14,7 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         """ Create user and client. """
         self.client = Client()
-        self.admin_user = get_user_model.objects.create_superuser(
+        self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
             password='testpass123'
         )
@@ -32,3 +32,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """ Test the edit user page works. """
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """ Test the create user page works. """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
